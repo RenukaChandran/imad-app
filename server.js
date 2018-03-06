0f2bf925f49406345dd8f6204042ae4a1bee2297
RenukaChandran <36416934+RenukaChandran@users.noreply.github.com>
2018-03-06 19:59:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,16 +15,46 @@ var article-one = {
          </p> `
 };
 
-function createtemplate (dataobj) {
+function createTemplate (dataobj) {
     var title = dataobj.title;
     var heading = dataobj.heading;
     var content = dataobj.content;
+
     var htmltemplate = `
-    
-}    
+    <html>
+    <head>
+        <title>
+            $(title)
+        </title>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link href='/ui/style.css' ref='stylesheet' />
+    </head>
+    <body>
+        <div class='art1'>
+            <div>
+                <a href='/'>HOME</a>
+            </div>
+            <hr>
+            <h2>
+                $(heading)
+            </h2>
+            <h3>
+                $(author)
+            </h3>
+            <div>
+               $(content)
+            </div>
+        </div>
+    </body>
+</html> `;
+
+return htmlTemplate;
+}
+
+
 }
 app.get('/article-one',function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(article-one));
 });
  
 app.get('/', function (req, res) {
