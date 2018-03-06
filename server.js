71d97ba5c64f6a36cd7dd2769fa619d88997235b
RenukaChandran <36416934+RenukaChandran@users.noreply.github.com>
2018-03-06 18:46:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/article-one',function (req, res) {
-    res.send("article one is served here!!!!!");
-})
+   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
  
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
