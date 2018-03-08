f314e8a921f72864bd25e02e79ffb91091e6f4ba
RenukaChandran <36416934+RenukaChandran@users.noreply.github.com>
2018-03-08 21:14:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML ="I have changed the content with javascript main file";
 
+var img = document.getElementById("madi");
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+}
