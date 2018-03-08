24c1aae1fd0f0be77afff813b1106889fe923f91
RenukaChandran <36416934+RenukaChandran@users.noreply.github.com>
2018-03-08 20:40:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ function createTemplate (dataobj) {
     <html>
     <head>
         <title>
-            ${title}
+            ${title}
         </title>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link href='/ui/style.css' ref='stylesheet' />
